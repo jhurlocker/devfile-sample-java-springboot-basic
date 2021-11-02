46ba418bef4f1177845553851046bf31fb268489
John Hurlocker <jhurlocker@gmail.com>
2021-11-02 07:41:59 -0600

    adding value to get env var

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
+    @Value("${var_flag}")
+    private String varFlag;
+    
     @RequestMapping("/")
     String home() {
+        System.out.println("==== VAR FLAG ==== " + varFlag);
         System.out.println("==== ENV VAR ==== " + System.getProperty("VAR_FLAG"));
         return "Hello World!";
     }
